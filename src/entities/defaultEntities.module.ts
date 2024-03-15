811e2be7d881cc2ef7921c277f1f07c07ec91920
AlfaGruisPL <alfagruis@gmail.com>
2024-03-15 13:28:30 +0100

    work

--- src/entities/defaultEntities.module.ts
+++ src/entities/defaultEntities.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './default/users/users.module';
 import { TokensModule } from './default/tokens/tokens.module';
-import { AspectsModule } from './aspects/aspects.module';
-import { ProjectsModule } from './projects/projects.module';
+import { WallpapersModule } from './wallpapers/wallpapers.module';
 
 @Module({
-  imports: [UsersModule, TokensModule, AspectsModule, ProjectsModule],
+  imports: [UsersModule, TokensModule, WallpapersModule],
   controllers: [],
-  exports: [UsersModule, TokensModule, AspectsModule, ProjectsModule],
+  exports: [UsersModule, TokensModule],
 })
 export class DefaultEntitiesModule {}
--- src/entities/wallpapers/entities/wallpaper.entity.ts
+++ src/entities/wallpapers/entities/wallpaper.entity.ts
@@ -1,28 +1,19 @@
 import {
   Column,
   CreateDateColumn,
-  Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity({ name: 'aspects' })
-export class AspectEntity {
+export class Wallpaper {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'text' })
-  title: string;
-
-  @Column({ type: 'text' })
-  content: string;
-
-  @Column({ type: 'text' })
-  shortDescription: string;
+  @Column()
+  name: string | null;
 
   @Column()
-  imageId: number;
-
+  price: number;
   @CreateDateColumn()
   createTime: Date;
 
